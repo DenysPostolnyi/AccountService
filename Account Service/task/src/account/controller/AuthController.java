61c134e63f3f7cc4c9fac99cdd049a36b8c87dc8
DenysPostolnyi <postlden44@gmail.com>
2023-03-19 18:24:28 +0200

    .

--- Account Service/task/src/account/controller/AuthController.java
+++ Account Service/task/src/account/controller/AuthController.java
@@ -6,15 +6,12 @@ import account.urils.ErrorResponse;
 import account.urils.IncorrectDataError;
 import account.urils.PersonMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/auth")
